829273d2a36300d957db2443c57854452ddd861e
jeeftaibrahim <jeeftaif@gmail.com>
2026-01-12 09:48:44 +0700

    tambah validasi nilai

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿int nilai;
+﻿using System.Net.Mail;
+
+int nilai;
 string nama;
 
 Console.WriteLine("Aplikasi Nilai Siswa");
@@ -8,3 +10,8 @@ nama = Console.ReadLine();
 
 Console.Write("Input nilai: ");
 nilai = int.Parse(Console.ReadLine());
+
+if (nilai >= 75)
+    Console.WriteLine("SELAMAT ANDA LULUS");
+else
+    Console.WriteLine("MAAF ANDA BELUM LULUS");
